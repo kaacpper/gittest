53f8288a9ae46fa8eafa2462ce1b0d558b90561b
kaacpper <kaacpper@gmail.com>
2017-03-08 12:36:13 +0100

    Szyfr cezara python kl2ag1

---
+++ szyfr_cezara.cpp
@@ -0,0 +1,34 @@
+/*
+ * szyfr_cezara.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+void szyfruj(char tekst[], int klucz) {
+        klucz = klucz % 26;
+        int i = 0; // indeks
+        while (tekst[i] != '\0') {
+            if ((int)tekst[i] + klucz > 122)
+                tekst[i] = (char)((int)tekst[i] + klucz - 26)
+            else
+            i++;
+    }
+}
+
+
+int main(int argc, char **argv)
+{
+    char tekst[100];
+    int klucz = 3;
+    cout << "Podaj tekst:" << endl;
+    cin >> tekst;
+    cout << "Podaj klucz:" << endl;
+    cin >> klucz;
+    szyfruj(tekst, klucz)
+    cout << tekst;
+
+	return 0;
+}
+
