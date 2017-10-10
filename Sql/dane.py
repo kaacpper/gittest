c6115676cae8cd9bd7f4a26e862e79584bb18be1
kl3ag1 <kl3ag1@komp12.lo1cg.org>
2017-10-10 12:32:17 +0200

    baza sqlite3

--- Sql/dane.py
+++ Sql/dane.py
@@ -5,19 +5,42 @@
 
 import csv
 
-def dane_z_pliku(plik):
+def dane_z_pliku(plik, delimiter='\t'):
     dane = [] #pusta lista
     with open(plik,'r') as plikcsv:
-        tresc = csv.reader(plikcsv, delimiter='\t')
+        tresc = csv.reader(plikcsv, delimiter=delimiter)
     
         for rekord in tresc:
             dane.append(rekord)
-    print(dane)
+    
+    return dane
 
 
+def wyczysc_dane(dane, pole):
+    for i, rekord in enumerate(dane):
+        el = rekord[pole]
+        el = el.replace('zł', '')
+        el = el.replace(' ', '')
+        el = el.replace(',', '.')
+        rekord[pole] = el
+        dane[i] = rekord
+
+    return dane
 
 def main(args):
-    dane_z_pliku('pracownicy.txt')
+    premia = dane_z_pliku('premia.txt')
+    premia = wyczysc_dane(premia, 1)
+    
+    dzial = dane_z_pliku('dział.txt')
+    
+    
+    pracownicy = dane_z_pliku('pracownicy.txt')
+    pracownicy = wyczysc_dane(pracownicy, 5)
+    
+    
+    print(premia)
+    
+    
     return 0
 
 if __name__ == '__main__':
