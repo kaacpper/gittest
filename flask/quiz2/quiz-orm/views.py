550062b09ff06b7c50808326280eb11bb12272a1
kl3ag1 <kl3ag1@komp12.lo1cg.org>
2017-11-21 12:35:49 +0100

    strony

--- flask/quiz2/quiz-orm/views.py
+++ flask/quiz2/quiz-orm/views.py
@@ -1,10 +1,22 @@
-# -*- coding: utf-8 -*-
-# quiz-orm/views.py
-
-from flask import render_template
+from flask import render_template, request
 from app import app
+from models import Pytanie, Odpowiedz
+from forms import *
 
 
 @app.route('/')
 def index():
+    """Strona główna"""
     return render_template('index.html')
+
+
+@app.route('/formularz', methods=['GET', 'POST'])
+def formularz():
+    if request.method == 'POST':
+        print(request.form)
+        imie = request.form['imie']
+        nazwisko = request.form['nazwisko']
+        wiek = request.form['wiek']
+        flash('Imię i nazwisko: %s %s' % (imie, nazwisko) 'kom')
+    dane = [imie, nazwisko, wiek]
+    return render_template('formularz.html', dane=dane)
