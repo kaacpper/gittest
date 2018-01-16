bf87e447d67910b8c655117a541a85e0fcbe73bb
kaacpper <kaacpper@gmail.com>
2018-01-16 14:20:19 +0100

    tabdynamiczne

--- Formatowanie/tabdynamiczne.cpp
+++ Formatowanie/tabdynamiczne.cpp
@@ -6,6 +6,8 @@
 
 
 #include <iostream>
+#include <iomanip> // setw()
+#include <cstdlib> // srand(), rand()
 
 using namespace std;
 
@@ -36,11 +38,53 @@ int tab1W() {
     return 0;
 }
 
+void wypelnij2W(int **tab, int w, int k) {
+    srand(time(NULL)); // inicjacja generatora liczb pseudolosowych
+    for(int i = 1; i < w; i++) {
+        for(int j = 1; j < k; j++) {
+            //cout << i << j << endl;
+            tab[i][j] = i * j;
+            cout << setw(4) << tab[i][j];
+        }
+        cout << endl;
+    }
+}
+
+int tab2W() {
+    int w, k, i;
+    cout << "Ile wierszy i kolumn podasz? ";
+    cin >> w >>k;
+    int **tab; //deklaracja wskaźnika do wskaźnika
+    
+    try {
+        tab = new int *[w]; // utworzenie tablicy wskaźników
+    } catch(bad_alloc) {
+        cout << "Za mało pamięci!";
+        return 1;
+    }
+    
+    for(i = 0; i < w; i++) {
+        try {        
+            tab[i] = new int [k]; // utworzenie tablicy liczb całkowitych
+        } catch(bad_alloc) {
+            cout << "Za mało pamięci!";
+            return 1;
+        }
+    }
+    wypelnij2W(tab, w, k);
+    return 0;        
+}
+
+
+
+
+
+
 
 int main(int argc, char **argv)
 {
-    
-    tab1W();
+    tab2W();
+    //tab1W();
 	return 0;
 }
 
