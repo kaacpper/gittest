df78bc546c758cbd691b289c378c0eb5f3c632f1
kaacpper <kaacpper@gmail.com>
2018-01-30 14:20:29 +0100

    onp

--- Formatowanie/tabdynamiczne.cpp
+++ Formatowanie/tabdynamiczne.cpp
@@ -1,90 +1,23 @@
 /*
  * tabdynamiczne.cpp
- * 
+ *
  * Copyright 2018  <>
  */
 
 
 #include <iostream>
-#include <iomanip> // setw()
-#include <cstdlib> // srand(), rand()
 
 using namespace std;
 
-void wprowadzDane(int *t, int ile) {
-    for(int i=0; i< ile; i++) {
-        cout << "Podaj liczbę: ";
-        //cin >> t[i];
-        cout << "Adres komórki: " << (t + i) << endl;
-        cin >> *(t + i);
-    }
-}
-
-
 int tab1W() {
-    int ile = 0;
-    cout << "Ile liczb podasz? " << endl;
-    cin >> ile;
-    
-    try {
-        int *tab;
-        tab = new int[ile];
-        wprowadzDane(tab, ile);
-    } catch(bad_alloc) {
-        cout << "Za mało pamięci!";
-        return 1;
-    }
-    
-    return 0;
+	cout << "Ile liczb podasz? " << endl;
 }
-
-void wypelnij2W(int **tab, int w, int k) {
-    srand(time(NULL)); // inicjacja generatora liczb pseudolosowych
-    for(int i = 1; i < w; i++) {
-        for(int j = 1; j < k; j++) {
-            //cout << i << j << endl;
-            tab[i][j] = i * j;
-            cout << setw(4) << tab[i][j];
-        }
-        cout << endl;
-    }
-}
-
-int tab2W() {
-    int w, k, i;
-    cout << "Ile wierszy i kolumn podasz? ";
-    cin >> w >>k;
-    int **tab; //deklaracja wskaźnika do wskaźnika
-    
-    try {
-        tab = new int *[w]; // utworzenie tablicy wskaźników
-    } catch(bad_alloc) {
-        cout << "Za mało pamięci!";
-        return 1;
-    }
-    
-    for(i = 0; i < w; i++) {
-        try {        
-            tab[i] = new int [k]; // utworzenie tablicy liczb całkowitych
-        } catch(bad_alloc) {
-            cout << "Za mało pamięci!";
-            return 1;
-        }
-    }
-    wypelnij2W(tab, w, k);
-    return 0;        
-}
-
-
-
-
-
-
 
 int main(int argc, char **argv)
 {
-    tab2W();
-    //tab1W();
+
+    tab1W();
+
 	return 0;
 }
 
---
+++ python/onp_stos.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def main(args):
+    stos = []
+    onp = input("Podaj wyrażenie ONP, oddzielając operandy i operatory spacjami:\n")
+
+    onp = onp.split(" ")
+
+    for wyraz in onp:
+        if wyraz.isdigit():
+            stos.append(wyraz)
+        else:
+            a = stos.pop()
+            b = stos.pop()
+            stos.append(eval(b + wyraz + a))
+
+
+
+    print("Wynik: ", stos.pop())
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
